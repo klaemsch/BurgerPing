04f282aef3374507b8a40e3e5e44c23c3fdc019b
klaemsch <klemens.wohlfarth@stud.uni-hannover.de>
2022-01-13 18:10:09 +0100

    new logo + title + favicon

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,5 +4,5 @@
 
 <header class="flex flex-row items-center border-b-2 border-gray-100 py-3">
     <img src="./burger.png" alt="BurgerPing Logo" width={50} class="rounded" />
-    <Title>BurgerPing</Title>
+    <!--<Title>BurgerPing</Title>-->
 </header>
