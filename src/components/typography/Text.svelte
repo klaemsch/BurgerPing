0ffdc06cd5fdd850d022ebe6115d7caa95252b59
Jens <j.koeneke@stud.uni-hannover.de>
2022-01-13 19:09:11 +0100

    add: dishes selection completed

--- src/components/typography/Text.svelte
+++ src/components/typography/Text.svelte
@@ -1,3 +1,19 @@
-<p class="font-sans">
+<script lang="ts">
+    export let extraStyle: boolean = false;
+    const extra: string = "font-sans extra";
+</script>
+
+<p class={extraStyle ? extra : "font-sans"}>
     <slot></slot>
 </p>
+
+<style>
+
+.extra {
+    padding-right: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>
