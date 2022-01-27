3b0693714b92037e4544a5358dbba255e63a69b2
klaemsch <54587753+klaemsch@users.noreply.github.com>
2022-01-27 17:56:25 +0100

    routing admin/cusomer view

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import GlobalStyle from "./Style.svelte";
 	import Header from "./components/Header.svelte";
+	import { Router, Link, Route } from "svelte-routing";
 	import Customer from "./pages/Customer.svelte";
 	import Admin from "./pages/Admin.svelte";
 </script>
@@ -8,5 +9,8 @@
 <GlobalStyle />
 <main class="flex flex-col h-screen">
 	<Header />
-	<Admin />
+	<Router>
+		<Route path="/" component={Customer} />
+		<Route path="/admin" component={Admin} />
+	</Router>
 </main>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
     import Button from "./input/Button.svelte";
     import Title from "./typography/Title.svelte";
+    import { Router, navigate } from "svelte-routing";
+
+    let label: string = "Admin";
+
+    const navigator = () => {
+        console.log(label)
+        if (window.location.pathname == "/admin") {
+            navigate("/", { replace: false });
+            label = "Admin";
+        } else if (window.location.pathname == "/") {
+            navigate("/admin", { replace: false });
+            label = "Kundenansicht";
+        } else {
+            console.log("This shouldnt be possible!")
+        }
+    }
+
 </script>
 
 <header
     class="flex flex-row gap-3 items-center border-b-2 border-gray-100 p-4 justify-between"
 >
     <img src="./burger.png" alt="BurgerPing Logo" width={50} class="rounded" />
-    <Title>Reservierung</Title>
-    <Button label="Login" onclick={() => {}} />
+    <Button label={label} onclick={navigator} />
 </header>
